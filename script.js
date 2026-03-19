6de524806889a72baa33bd6ef2e8e467bd105a1c
nalaunalou-dev <nala.una.lou@gmail.com>
2026-03-20 00:43:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -22,21 +22,21 @@ function showResult() {
 
   if (scoreCru > scoreDoux) {
     result.innerHTML = `
-      <h2>Version CRUE</h2>
-      <p>Flow découpé, vocal fry agressif.</p>
-      <a href="https://youtube.com" target="_blank">YouTube</a><br>
-      <a href="https://spotify.com" target="_blank">Spotify</a><br>
+      <h2>Version CHROME </h2>
+      <p>Dans une grotte sombre où l'air semble suspendu, vous vous sentez observé, la bête rôde. </p>
+      <a href="https://www.youtube.com/watch?v=JGgbWu4WDDw&list=OLAK5uy_k4VzMNh5QwFC7a3HhJw-vFHGuVSd9hzLU" target="_blank">YouTube</a><br>
+      <a href="https://open.spotify.com/intl-fr/track/0MZY7VUQzUoC4ibyv1KiWE" target="_blank">Spotify</a><br>
       <a href="https://deezer.com" target="_blank">Deezer</a><br>
-      <a href="https://music.apple.com" target="_blank">Apple Music</a>
+      <a href="https://music.apple.com/fr/album/la-b%C3%AAte-chrome/1880880827?i=1880880829" target="_blank">Apple Music</a>
     `;
   } else {
     result.innerHTML = `
-      <h2>Version MÉLODIEUSE</h2>
-      <p>Harmonies, douceur, lignes chantées.</p>
-      <a href="https://youtube.com" target="_blank">YouTube</a><br>
-      <a href="https://spotify.com" target="_blank">Spotify</a><br>
+      <h2>Version Gold </h2>
+      <p>Dans une prairie en plein jour, le monstre n'est pas celui que vous croyez. Les petites voix dans votre tête vont-elles se coordonner ? .</p>
+      <a href="https://www.youtube.com/watch?v=jDt-CiZFL3E&list=OLAK5uy_k4VzMNh5QwFC7a3HhJw-vFHGuVSd9hzLU&index=2" target="_blank">YouTube</a><br>
+      <a href="https://open.spotify.com/intl-fr/track/52A7NP9FTFszAv194su7Zj" target="_blank">Spotify</a><br>
       <a href="https://deezer.com" target="_blank">Deezer</a><br>
-      <a href="https://music.apple.com" target="_blank">Apple Music</a>
+      <a href="https://music.apple.com/fr/song/la-b%C3%AAte-gold/1880880832" target="_blank">Apple Music</a>
     `;
   }
 }
